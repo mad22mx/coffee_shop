966e51068eff2c1bcd7f596e481855a090ba25b1
AhmetHayrioglu <150132527+AhmetHayrioglu@users.noreply.github.com>
2024-01-02 20:57:32 +0300

    Update Main.java
    fixed some typing error

--- src/coffee_shop/Main.java
+++ src/coffee_shop/Main.java
@@ -1,7 +1,7 @@
 package coffee_shop;
 import java.util.*;
 public class Main {
-	static String[] options = {"Cappucino","Latte","Frappuccino","Espresso"
+	static String[] options = {"Cappuccino","Latte","Frappuccino","Espresso"
 		                                            ,"Americano"};
 	public static void main(String[] args) {
 		Drinks_machine machine = Drinks_machine.getInstance();

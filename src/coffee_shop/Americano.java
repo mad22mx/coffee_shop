6146f03df63bd7e8ea66efe67e7ed9b2a2b78bb8
AhmetHayrioglu <150132527+AhmetHayrioglu@users.noreply.github.com>
2024-01-02 20:56:42 +0300

    Update Americano.java
    Fixed Syntax erorr

--- src/coffee_shop/Americano.java
+++ src/coffee_shop/Americano.java
@@ -5,7 +5,7 @@ public class Americano implements Coffee {
 	@Override
 	public void make_coffee() {
 		// TODO Auto-generated method stub
-		System.out.println("your Americano is being made")
+		System.out.println("your Americano is being made");
 	}
 
 }
